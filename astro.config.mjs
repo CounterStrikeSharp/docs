ed2a362a69ee30cae4f4fa8ed1dbeb9cdc8b8588
Michael Wilson <shortguy014@gmail.com>
2024-04-09 20:29:30 +1000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,64 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'CounterStrikeSharp',
+      logo: { src: './src/assets/logo.svg' },
+      favicon: './src/assets/logo.svg',
+      customCss: [
+        '@fontsource/ibm-plex-sans/400.css',
+        '@fontsource/ibm-plex-sans/500.css',
+        '@fontsource/ibm-plex-sans/700.css',
+        '@fontsource/jetbrains-mono/400.css',
+        '@fontsource/jetbrains-mono/600.css',
+        './src/styles/custom.css',
+      ],
+      head: [
+        {
+          tag: 'script',
+          attrs: {
+            src: 'https://gc.zgo.at/count.js',
+            'data-goatcounter': 'https://cssharp.goatcounter.com/count',
+            async: true,
+          },
+        },
+      ],
+      social: {
+        github: 'https://github.com/roflmuffin/CounterStrikeSharp',
+        discord: 'https://discord.gg/eAZU3guKWU'
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+        },
+        {
+          label: 'Features',
+          autogenerate: { directory: 'features' },
+        },
+        {
+          label: 'Admin Framework',
+          autogenerate: { directory: 'admin-framework' },
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+      editLink: {
+        baseUrl:
+          'https://github.com/roflmuffin/CounterStrikeSharp/edit/main/docs/',
+      },
+    }),
+  ],
+  base: '/',
+  site: 'https://docs.cssharp.dev',
+  markdown: {
+    shikiConfig: {
+      wrap: false,
+    },
+  },
+});
